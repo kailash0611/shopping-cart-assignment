5b563895cc0144a20d238120ee6bf62a3d920737
Kailash Rampariya <kailashrampariya@gmail.com>
2025-02-08 03:02:36 +0530

    Route details added

---
+++ src/routes/cartRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const cartController = require("../controllers/cartController");
+
+const router = express.Router();
+
+router.post("/add", cartController.addToCart);
+router.get("/", cartController.getCart);
+
+module.exports = router;
